7b11bde72b92960c72bda82a527746ac17bb4367
Michael DESKTOP <tan00060@algonquinlive.com>
2021-05-28 13:12:54 -0400

    mechanical keyboard

---
+++ ming6macro/readme.md
@@ -0,0 +1,20 @@
+# ming6macro
+
+![ming6macro assembled](https://i.imgur.com/50zzoPy.jpg)
+![ming6macro insides](https://i.imgur.com/RZ7BMlZ.jpg)
+
+This is a handwired numpad using an Arduino Pro Micro and a 3D printed case from thingiverse.
+
+* Keyboard Maintainer: [mingwahlu](https://github.com/tan00060)
+* Hardware Supported: Ardiuno Pro Micro ATmega32U4: [ArdiunoProMicro](https://www.amazon.ca/KeeYees-ATmega32U4-Development-Microcontroller-Bootloader/dp/B07FXCTVQP/ref=sr_1_3?dchild=1&keywords=arduino+pro+micro&qid=1598072684&sr=8-3)
+* Hardware Availability: 3D Print of the numpad case: [Numpad](https://www.thingiverse.com/thing:3393103)
+
+Make example for this keyboard (after setting up your build environment):
+
+    make handwired/mingwahlu/ming6macro:default
+
+Flashing example for this keyboard:
+
+    make handwired/mingwahlu/ming6macro:default:flash
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
---
+++ mingwahlumacro/keymaps/default/keymap.c
@@ -0,0 +1,118 @@
+/* Copyright 2020 mingwahlu
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+// Defines names for use in layer keycodes and the keymap
+enum layer_names {
+    _BASE,
+    _FN
+};
+
+// Defines the keycodes used by our macros in process_record_user
+enum custom_keycodes {
+    QMKBEST = SAFE_RANGE,
+    QMKURL,
+    M_NUTS,
+    M_UMARUBM,
+    M_UMARUEAT,
+    M_UMARUMAD,
+    M_UMARUHAPPY,
+    M_UMARUSAD,
+    M_UMARUGAME,
+    M_UMARUSLEEP
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* Base */
+    [_BASE] = LAYOUT(
+        M_UMARUHAPPY, M_UMARUSAD, M_UMARUMAD, MO(_FN),
+        M_UMARUEAT, M_UMARUGAME, M_UMARUBM, M_UMARUSLEEP
+    ),
+    [_FN] = LAYOUT(
+        LALT(KC_F4), KC_F5, RESET, MO(_FN),
+        LCTL(KC_W), C(S(KC_T)), LCTL(KC_T), LALT(KC_TAB)
+    )
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case QMKBEST:
+            if (record->event.pressed) {
+                // when keycode QMKBEST is pressed
+                SEND_STRING("QMK is the best thing ever!");
+            } else {
+                // when keycode QMKBEST is released
+            }
+            break;
+        case QMKURL:
+            if (record->event.pressed) {
+                // when keycode QMKURL is pressed
+                SEND_STRING("https://qmk.fm/\n");
+            } else {
+                // when keycode QMKURL is released
+            }
+            break;
+        case M_NUTS:
+            if (record->event.pressed) {
+                SEND_STRING("hello, mingwahlu!\n");
+            } else {
+            }
+            break;
+        case M_UMARUBM:
+            if(record->event.pressed) {
+                SEND_STRING("https://imgur.com/NvKoahR\n");
+            } else {
+            }
+            break;
+        case M_UMARUEAT:
+            if(record->event.pressed) {
+                SEND_STRING("https://tenor.com/view/ramen-slurp-anime-umaru-cute-gif-16722331\n");
+            } else {
+            }
+            break;
+        case M_UMARUMAD:
+            if(record->event.pressed) {
+                SEND_STRING("https://tenor.com/view/anime-umaru-mad-cute-small-girl-gif-5169671\n");
+            } else {
+            }
+            break;
+        case M_UMARUHAPPY:
+            if(record->event.pressed) {
+                SEND_STRING("https://tenor.com/view/umaru-gif-4927713\n");
+            } else {
+            }
+            break;
+        case M_UMARUSAD:
+            if(record->event.pressed) {
+                SEND_STRING("https://tenor.com/view/umaru-crying-tears-gif-5497252\n");
+            } else {
+            }
+            break;
+        case M_UMARUGAME:
+            if(record->event.pressed) {
+                SEND_STRING("https://tenor.com/view/mash-buttons-press-himouto-umaru-chan-anime-gif-17872461\n");
+            } else {
+            }
+            break;
+        case M_UMARUSLEEP:
+            if(record->event.pressed) {
+                SEND_STRING("https://tenor.com/view/umaru-himouto-sleeping-dreaming-gif-4769761\n");
+            } else {
+            }
+            break;
+    }
+    return true;
+}
